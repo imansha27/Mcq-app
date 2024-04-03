2a20b85a77d7d1892d4bb5735435c5f397092c6d
Pallimulla K Imansha <89239162+imansha27@users.noreply.github.com>
2024-04-04 00:44:33 +0530

    discussion thread

--- Application/Server/routes/Discussions.js
+++ Application/Server/routes/Discussions.js
@@ -8,43 +8,41 @@ const upload = require('../middlewares/fileUploadMiddleware');
 
 
 //submit new discussion
+router.post('/newdiscuss', verifyToken, upload, async (req, res) => {
+    try {
+        const username = req.username;
+        const { title, content } = req.body;
 
-router.post('/newdiscuss',verifyToken,upload,async(req,res)=>{
-    try{
-        const username =req.username;
-
-        const{title,content}=req.body;
-
-        let image=null;
-        if(req.file){
-            image={
-                filename:req.files.filename,
-                path:req.file.path
+        let image = null;
+        if (req.file) {
+            image = {
+                filename: req.file.filename,
+                path: req.file.path
             };
         }
-        if (!title||!content){
-            return res.status(400).json({error:"Both Title and Content are required"});
+
+        if (!title || !content) {
+            return res.status(400).json({ error: "Both Title and Content are required" });
         }
 
-        const newdiscuss=new Discussion({
+        const newdiscuss = new Discussion({
             title,
             content,
             image,
-            author:username,
+            author: username,
         });
 
-        const existingdiscuss = await Discussion.findOne({title});
-        if(existingdiscuss){
-            return res.status(400).json({error:"There's a;ready a discussion with this title"});
+        const existingdiscuss = await Discussion.findOne({ title });
+        if (existingdiscuss) {
+            return res.status(400).json({ error: "There's already a discussion with this title" });
         }
         await newdiscuss.save();
-        return res.status(200).json({success:"New Discussion started!"});
-
-
-    }catch(error){
-        return res.status(400).json({error:error.message});
+        return res.status(200).json({ success: "New Discussion started!" });
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
     }
 })
+
 
 
 
