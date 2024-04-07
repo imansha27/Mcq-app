d29e03c6b759bb91a5a5a3ca62b0a825abeeaac4
Pallimulla K Imansha <89239162+imansha27@users.noreply.github.com>
2024-04-08 01:11:21 +0530

    quiz page

--- Application/Server/routes/Questions.js
+++ Application/Server/routes/Questions.js
@@ -8,13 +8,13 @@ const upload = require('../middlewares/fileUploadMiddleware'); // Require file u
 // submit new question
 router.post('/submitques', verifyToken, upload, async (req, res) => {
     try {
-        // Retrieve the username from the request object
+      
         const username = req.username;
 
-        // Check if all required fields are provided
+       
         const { Question, Choice1, Choice2, Choice3, Choice4, Choice5, Correctans, Category, source } = req.body;
         
-        // Check if the image file exists
+        //  image file exists
         let image = null;
         if (req.file) {
             image = {
@@ -37,26 +37,49 @@ router.post('/submitques', verifyToken, upload, async (req, res) => {
             Correctans,
             Category,
             source,
-            image, // Assign the image object
-            submitby: username, // Set the submitby field to the username
+            image, 
+            submitby: username,
         });
-        //console.log(newQues.image.path);
+     
 
-        // Check if the question already exists
+     
         const existingQuestion = await sques.findOne({ Question });
         if (existingQuestion) {
             return res.status(400).json({ error: "Question already exists" });
         }
 
-        // Save the new question to the database
+       
         await newQues.save();
         
-        // Send success response
+  
         return res.status(200).json({ success: "Question submitted successfully" });
     } catch (error) {
-        // Handle any errors that occur during the process
+       
         return res.status(400).json({ error: error.message });
     }
 });
+
+
+
+
+
+
+
+
+//pass questions for the quiz page
+
+router.get('/quizques',verifyToken,async(req,res)=>{
+    try {
+        const question=await sques.find().limit(15);
+        res.json(question);
+    } catch (error) {
+        res.status(500).json({message:error.message});
+    }
+})
+
+
+
+
+
 
 module.exports = router;
